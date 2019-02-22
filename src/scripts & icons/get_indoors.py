aa5b117ed1fcde80808f6bb819f7adddb5d18773
ronguest <ronaldg@PowerHouse.local>
2019-02-22 12:29:01 -0600

    get_indoors new functional with AW

--- src/scripts & icons/get_indoors.py
+++ src/scripts & icons/get_indoors.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python
 
-import credentials
-
 import urllib2
 import json
-#f = open("conditions.json", "r")
-#
-# Get the current conditions via a URL from weatherunderground
-#
+#import keys
+import credentials
 
-#print "WU key ="+credentials.WU_API_KEY+"-"
 URL='https://api.ambientweather.net/v1/devices/08:D8:33:44:63:13?applicationKey='+credentials.AW_APP_KEY+'&apiKey='+credentials.AW_API_KEY+'&limit=1'
-#print URL
-#exit()
 
 f = urllib2.urlopen(URL)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 
-#print json.dumps(parsed_json, indent=4)
-#print parsed_json
-for key in parsed_json:
-    if key["tempinf"]:
-        #print "Found it!"
-        print "Indoor temperature is",key["tempinf"]
-
-#temp_f = parsed_json["tempinf"]
-#print temp_f
-exit()
 #
 # Write the temperature
 #
-temp_f = parsed_json['AW_my_data_json'][0]['tempinf']
-#output = open("/mnt/sd/inside_temp.txt", "w")
-print >> output, temp_f
+for key in parsed_json:
+    if key["tempinf"]:
+        tempin_f = key["tempinf"]
+output = open("/mnt/sd/inside_temp.txt", "w")
+#output = open("inside_temp.txt", "w")
+print >> output, tempin_f
 output.close()
+
 #
 # Write humidity
 #
-humidity = parsed_json['current_observation'][0]['humidityin']
-#output = open("/mnt/sd/inside_humidity.txt", "w")
-print >> output, humidity
+for key in parsed_json:
+    if key["humidityin"]:
+        humidityin = key["humidityin"]
+output = open("/mnt/sd/inside_humidity.txt", "w")
+#output = open("inside_humidity.txt", "w")
+print >> output, humidityin
 output.close()
 
 f.close()
