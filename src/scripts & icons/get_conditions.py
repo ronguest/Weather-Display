ff03a20e1fa1bb81c5b004310ae235216ef264b7
ronguest <ronaldg@PowerHouse.local>
2019-02-27 09:33:13 -0600

    Convert to new Weather Underground/Weather Channel/IBM API
    New API key, new APIs, new icons/codes

--- src/scripts & icons/get_conditions.py
+++ src/scripts & icons/get_conditions.py
@@ -5,7 +5,7 @@ import json
 #import keys
 import credentials
 
-URL='http://api.wunderground.com/api/'+credentials.WU_API_KEY+'/conditions/q/pws:KTXALLEN99.json'
+URL='https://api.weather.com/v2/pws/observations/current?stationId=KTXALLEN99&format=json&units=e&apiKey='+credentials.WU_API_KEY
 #f = urllib2.urlopen('http://api.wunderground.com/api/$WU_KEY/conditions/q/pws:KTXALLEN99.json')
 f = urllib2.urlopen(URL)
 json_string = f.read()
@@ -13,21 +13,23 @@ parsed_json = json.loads(json_string)
 #
 # Write the temperature
 #
-temp_f = parsed_json['current_observation']['temp_f']
+temp_f = parsed_json['observations'][0]['imperial']['temp']
 output = open("/mnt/sd/temperature.txt", "w")
 print >> output, temp_f
 output.close()
 #
 # Write humidity
 #
-humidity = parsed_json['current_observation']['relative_humidity']
+humidity = parsed_json['observations'][0]['humidity']
 output = open("/mnt/sd/humidity.txt", "w")
 print >> output, humidity
 output.close()
 #
 # Write conditions icon file name
 #
-icon = parsed_json['current_observation']['icon']
+# The new WU API does not provide icon info for current conditions
+#icon = parsed_json['current_observation']['icon']
+icon = 'unk'
 output = open("/mnt/sd/current_icon.txt", "w")
 print >> output, "/mnt/sd/iconset9/"+icon+".bmp"
 output.close()
--- src/scripts & icons/get_forecast.py
+++ src/scripts & icons/get_forecast.py
@@ -5,60 +5,63 @@ import json
 #import keys
 import credentials
 
-URL='http://api.wunderground.com/api/'+credentials.WU_API_KEY+'/forecast/q/TX/Allen.json'
+URL='https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=75013:US&units=e&language=en-US&format=json&apiKey='+credentials.WU_API_KEY
 f = urllib2.urlopen(URL)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 
 # Get today's high & low
-high = parsed_json['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit']
+high = parsed_json['temperatureMax'][0]
 #print "High is ", high
 output = open("/mnt/sd/todays_high.txt", "w")
 print >> output, high
 output.close()
 
-low = parsed_json['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit']
+low = parsed_json['temperatureMin'][0]
 output = open("/mnt/sd/todays_low.txt", "w")
 print >> output, low
 output.close()
 
 # Today's probability of precipitation
-pop = parsed_json['forecast']['simpleforecast']['forecastday'][0]['pop']
+pop = parsed_json['daypart'][0]['precipChance'][0]
 output = open("/mnt/sd/todays_pop.txt", "w")
 print >> output, pop
 output.close()
 
 # Today's short conditions phrase
-conditions = parsed_json['forecast']['simpleforecast']['forecastday'][0]['conditions']
+conditions = parsed_json['narrative'][0]
 output = open("/mnt/sd/todays_conditions.txt", "w")
 print >> output, conditions
 output.close()
 
 # Get tomorrow's high & low
-high = parsed_json['forecast']['simpleforecast']['forecastday'][1]['high']['fahrenheit']
+high = parsed_json['temperatureMax'][1]
 output = open("/mnt/sd/tomorrows_high.txt","w")
 print >> output, high
 output.close()
 
-low = parsed_json['forecast']['simpleforecast']['forecastday'][1]['low']['fahrenheit']
+low = parsed_json['temperatureMin'][1]
 output = open("/mnt/sd/tomorrows_low.txt", "w")
 print >> output, low
 output.close()
 
 # Tomorrow's probability of precipitation
-pop = parsed_json['forecast']['simpleforecast']['forecastday'][1]['pop']
+pop = parsed_json['daypart'][0]['precipChance'][2]
 output = open("/mnt/sd/tomorrows_pop.txt", "w")
 print >> output, pop
 output.close()
 
 # Tomorrow's short conditions phrase
-conditions = parsed_json['forecast']['simpleforecast']['forecastday'][1]['conditions']
+conditions = parsed_json['narrative'][1]
 output = open("/mnt/sd/tomorrows_conditions.txt", "w")
 print >> output, conditions
 output.close()
 
 # Tomorrow's short conditions icon
-icon = parsed_json['forecast']['simpleforecast']['forecastday'][1]['icon']
+# The WU Icon PNG/BMP folders contain the official icons corresponding to the iconCode
+icon = parsed_json['daypart'][0]['iconCode'][2]
 output = open("/mnt/sd/tomorrow_icon.txt", "w")
 print >> output, "/mnt/sd/iconset9/"+icon+".bmp"
 output.close()
+
+f.close()
